9b03ad9805b40446ca44d8fbd2c3bf75a98aff67
Daniel Cunha <daniel.cunha@Daniels-MacBook-Pro.local>
2023-02-09 19:10:30 -0800

    Migrations Setup

--- api/src/routes/testRouter.ts
+++ api/src/routes/testRouter.ts
@@ -6,47 +6,47 @@ const prisma = new PrismaClient()
 const router = Router()
 
 // POST - Create Record
-router.post('/', async (req, res) => {
-  const test = await prisma.test.create({
-    data: {
-      ...req.body,
-    },
-  })
+// router.post('/', async (req, res) => {
+//   const test = await prisma.test.create({
+//     data: {
+//       ...req.body,
+//     },
+//   })
 
-  res.json(test)
-})
+//   res.json(test)
+// })
 
-// GET - Retrieve Records
-router.get('/', async (_, res) => {
-  const testList = await prisma.test.findMany()
+// // GET - Retrieve Records
+// router.get('/', async (_, res) => {
+//   const testList = await prisma.test.findMany()
 
-  res.json(testList)
-})
+//   res.json(testList)
+// })
 
-// PUT - Update Record
-router.put('/', async (req, res) => {
-  const { id, ...data } = req.body
-  const test = await prisma.test.update({
-    where: {
-      id,
-    },
-    data,
-  })
+// // PUT - Update Record
+// router.put('/', async (req, res) => {
+//   const { id, ...data } = req.body
+//   const test = await prisma.test.update({
+//     where: {
+//       id,
+//     },
+//     data,
+//   })
 
-  res.json(test)
-})
+//   res.json(test)
+// })
 
-// DELETE - Delete Record
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params
+// // DELETE - Delete Record
+// router.delete('/:id', async (req, res) => {
+//   const { id } = req.params
 
-  const test = await prisma.test
-    .delete({
-      where: { id },
-    })
-    .catch((error) => error)
+//   const test = await prisma.test
+//     .delete({
+//       where: { id },
+//     })
+//     .catch((error) => error)
 
-  res.json(test)
-})
+//   res.json(test)
+// })
 
 export { router as testRouter }
