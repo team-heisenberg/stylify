b4721a16eb093b7b7a180b05725d188ce0074e45
Gabriel Dias <gaabrieldls@gmail.com>
2023-02-09 19:55:22 -0800

    feature: routes appointmentDetails, customers and review

---
+++ api/src/routes/reviewRouter.ts
@@ -0,0 +1,53 @@
+import { Router } from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const router = Router()
+
+// POST - Create Record
+
+router.post('/', async (req, res) => {
+  const review = await prisma.review.create({
+    data: {
+      ...req.body,
+    },
+  })
+
+  res.json(review)
+})
+
+// GET - Retrieve Records
+router.get('/', async (_, res) => {
+  const reviewList = await prisma.review.findMany()
+
+  res.json(reviewList)
+})
+
+// PUT - Update Record
+router.put('/', async (req, res) => {
+  const { reviewID, ...data } = req.body
+  const review = await prisma.review.update({
+    where: {
+      reviewID: Number (reviewID),
+    },
+    data,
+  })
+
+  res.json(review)
+})
+
+// DELETE - Delete Record
+router.delete('/:reviewID', async (req, res) => {
+  const { reviewID } = req.params
+
+  const review = await prisma.review
+    .delete({
+      where: { reviewID: Number (reviewID) },
+    })
+    .catch((error) => error)
+
+  res.json(review)
+})
+
+export { router as reviewRouter }
