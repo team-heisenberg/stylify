94a74cc2cc185ff1fd155e273dcdfa4f2154c5f0
Daniel Cunha <danielbrandaodacunha@gmail.com>
2023-03-14 20:29:27 -0700

    HB-143: Auth integration and routes

--- app/src/screens/HomeBusiness/HomeBusiness.tsx
+++ app/src/screens/HomeBusiness/HomeBusiness.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Heading3 } from "../../components/NormalText/FontTypes";
 import NormalText from "../../components/NormalText/NormalText";
@@ -6,7 +7,9 @@ import { signOut } from "firebase/auth";
 import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-const HomeBusiness: React.FC<NativeStackScreenProps<any>>  = ({ navigation }) => {
+const HomeBusiness: React.FC<NativeStackScreenProps<any>> = ({
+  navigation,
+}) => {
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -20,6 +23,14 @@ const HomeBusiness: React.FC<NativeStackScreenProps<any>>  = ({ navigation }) =>
         normalText="Home Business Screen"
         fontType={Heading3}
         textAlign="left"
+      />
+      <ButtonComponent
+        buttonText="Create New Appointment"
+        onPress={() =>
+          navigation.navigate("Create Appointment Business", {
+            title: "Create Appointment",
+          })
+        }
       />
       <ButtonComponent onPress={handleSignOut} buttonText="Sign Out" />
     </>
