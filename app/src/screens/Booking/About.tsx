c146ee851ff3b0cd5b535c0da2c0ef511a9401a3
Gabriel Dias <gaabrieldls@gmail.com>
2023-03-28 14:30:47 -0700

    feature: creating appointment wrong branch

---
+++ app/src/screens/Booking/About.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import NormalText from "../../components/NormalText/NormalText";
+
+const About = () => {
+  return (
+    <View>
+      <NormalText normalText="Test" />
+    </View>
+  );
+};
+
+export default About;
---
+++ app/src/screens/Booking/BookingSpecialists.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import NormalText from "../../components/NormalText/NormalText";
+
+const BookingSpecialists = () => {
+  return (
+    <View>
+      <NormalText normalText="Test" />
+    </View>
+  );
+};
+
+export default BookingSpecialists;
