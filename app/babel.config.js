0cd7721b71df253c4000cf8261058c40ab692019
Daniel Cunha <danielbrandaodacunha@gmail.com>
2023-04-12 22:40:22 -0700

    HB-226: Env variables hidden

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+  const plugins = [
+    [
+      "module:react-native-dotenv",
+      {
+        envName: "APP_ENV",
+        moduleName: "@env",
+        path: ".env",
+      },
+    ],
+  ]
+
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins
   };
 };
--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -1,22 +1,21 @@
-import axios, { AxiosInstance } from 'axios'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import axios, { AxiosInstance } from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { REACT_APP_DEV_API, REACT_APP_PROD_API } from "@env";
 
 export const createAxiosClient = async () => {
-  const jwtToken: string = await AsyncStorage.getItem("@stylify:token") || "";
+  const jwtToken: string = (await AsyncStorage.getItem("@stylify:token")) || "";
 
   const axiosClient = axios.create({
-    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:8080": "http://ec2-35-155-214-56.us-west-2.compute.amazonaws.com/api",
+    baseURL:
+      process.env.NODE_ENV === "development"
+        ? REACT_APP_DEV_API
+        : REACT_APP_PROD_API,
     headers: {
-      "Authorization": `Bearer ${jwtToken}`
-    }
-  })
+      Authorization: `Bearer ${jwtToken}`,
+    },
+  });
 
   return {
-    axiosClient
-  }
-}
-
-
-
-
-
+    axiosClient,
+  };
+};
